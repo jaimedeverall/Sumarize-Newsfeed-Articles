5fe496911ab6046366f16a1b7b5c75b65229b395
maximilianchang <maximilianachang@gmail.com>
2018-02-08 00:47:48 -0800

    added math for time to read

--- server_files/summary.go
+++ server_files/summary.go
@@ -3,12 +3,11 @@ package main
 import "os/exec"
 import "encoding/json"
 import "fmt"
+import "strings"
 
 func retrieveSummary(article_url string) map[string]interface{} {
 	cmd := exec.Command("./extraction_script.py", article_url) 
 	summary_bytes, _ := cmd.Output()
-	//summary_string := string(summary_bytes)
-	//article_data := summary_bytes 
 
 	raw := make( map[string]string )
 	valid := json.Valid(summary_bytes)
@@ -26,8 +25,11 @@ func retrieveSummary(article_url string) map[string]interface{} {
 	newArticle.url = raw["url"]
 	newArticle.publish_date = raw["publish_date"]
 
+	allWords := strings.Split(newArticle.text, " ")
+	num_words := len(allWords)
+
 	newArticle.reputability = 0 
-	newArticle.time_to_read = 0
+	newArticle.time_to_read = float64(num_words) / 275.0
 
 	return map[string]interface{}{"author_reputability": newArticle.reputability, "time_to_read": newArticle.time_to_read, 
 				"recap": newArticle.summary}
