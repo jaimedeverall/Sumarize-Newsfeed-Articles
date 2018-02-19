073d5c4b23f28b9ed53d47587242abb339b824fc
Jaime Deverall <jaime1deverall@gmail.com>
2018-02-19 12:00:26 -0800

    bug waiting for ads

--- basic-chrome-extension/test.js
+++ basic-chrome-extension/test.js
@@ -1,15 +1,28 @@
 console.log('test.js')
+const highlightsDivWidth = 30;
+const spacing = 5;
+const identifier = 'gisthighlights'
+var counter = 0;
 
 function getHighlights(url){
   console.log('getting highlights');
-  chrome.runtime.sendMessage({endpoint: "highlights", article_url: url}, function(response) {
-    var obj = JSON.parse(response);
-    var highlights = obj.highlights
-    createHighlights(highlights);
-  });
+  const highlights = JSON.parse(window.sessionStorage.getItem(identifier));
+  if(highlights === null){
+    chrome.runtime.sendMessage({endpoint: "highlights", article_url: url}, function(response) {
+      console.log('saving highlights');
+      var obj = JSON.parse(response);
+      var highlights = obj.highlights
+      window.sessionStorage.setItem(identifier, JSON.stringify(highlights));
+      createHighlights(highlights);
+    });
+  }else{
+    createHighlights(highlights)
+  }
 }
 
 function createHighlights(highlights){
+  var leftMostPosition = null
+  var matchedElements = []
   Object.keys(highlights).forEach(function(key){
     const sentence = highlights[key][0]
     const score = highlights[key][1]
@@ -17,11 +30,53 @@ function createHighlights(highlights){
       console.log(sentence);
       console.log(score);
       const selector = `h1:contains('${sentence}'), h2:contains('${sentence}'), h3:contains('${sentence}'), h4:contains('${sentence}'), h5:contains('${sentence}'), h6:contains('${sentence}'), p:contains('${sentence}')`;
-      $(selector).each(function(index, element){
-        console.log(element);
-      })
+      var matchedElement = $(selector).get(0)
+      if(matchedElement !== undefined){
+        console.log(matchedElement);
+        var position = $(matchedElement).offset();
+        if(leftMostPosition == null){
+          leftMostPosition = position.left;
+        }else{
+          if(position.left < leftMostPosition){
+            leftMostPosition = position.left;
+          }
+        }
+        matchedElements.push({element: matchedElement, score: score})
+      }
     }
   });
+
+  for(var i=0; i<matchedElements.length; i++){
+    var element = matchedElements[i].element;
+    var score = matchedElements[i].score;
+    var position = $(element).offset();
+    var height = $(element).height();
+    var highlightsDiv = createHighlightsDiv(height, score);
+    positionHighlightsDiv(highlightsDiv, leftMostPosition, position.top)
+    document.body.appendChild(highlightsDiv);
+  }
+
 }
 
-getHighlights(document.URL);
+function createHighlightsDiv(height, score){
+  var highlightsDiv = document.createElement('div');
+  $(highlightsDiv).css({height: height, width: highlightsDivWidth});
+  highlightsDiv.setAttribute('class', 'highlights_div');
+  return highlightsDiv
+}
+
+function positionHighlightsDiv(highlightsDiv, leftMost, top){
+  var left = leftMost - spacing - highlightsDivWidth;
+  $(highlightsDiv).css({top: top, left: left})
+}
+
+window.addEventListener("load", function(event){
+  var looper = setInterval(function(){
+    $('.highlights_div').remove();
+    getHighlights(document.URL);
+    counter++;
+    if (counter >= 30){
+      clearInterval(looper);
+    }
+  }, 1000);
+});
