0710b2cfeffca1bfdda68b5593e93d53dfcbe300
maximilianchang <maximilianachang@gmail.com>
2018-03-05 22:24:12 -0800

    finished login details

--- basic-chrome-extension/eventPage.js
+++ basic-chrome-extension/eventPage.js
@@ -1,3 +1,5 @@
+
+
 function parseRequest(request)  { 
   var parseStr = "";
   for (var key in request) {
@@ -16,18 +18,23 @@ function parseRequest(request)  {
 // requires end point + parameters
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
-    var parameter_string = parseRequest(request.parameters)
-    //var requestUrl = "http://localhost:8080/" + request.endpoint + "?" + parameter_string
-    var requestUrl = "http://35.230.103.160:80/" + request.endpoint + "?" + parameter_string
-
-    var xhr = new XMLHttpRequest();
-    xhr.open(request.request_type, requestUrl, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        sendResponse(xhr.response);
+    chrome.storage.sync.get("username", (user_name) => {
+      var user_name = chrome.runtime.lastError ? null : user_name[user_name]
+      if (user_name == null) { 
+        user_name = ""
       }
-    }
-    xhr.send();
-    return true
+      var parameter_string = parseRequest(request.parameters) + "&user_id=" + user_name
+    //var requestUrl = "http://localhost:8080/" + request.endpoint + "?" + parameter_string
+      var requestUrl = "http://35.230.103.160:80/" + request.endpoint + "?" + parameter_string
+
+      var xhr = new XMLHttpRequest();
+      xhr.open(request.request_type, requestUrl, true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          sendResponse(xhr.response);
+        }
+      }
+      xhr.send();
+    });
   }
 );
